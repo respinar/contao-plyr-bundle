4663b1200aabb6906056088b20abfd38e76894fe
Hamid Peywasti <hamid@respinar.com>
2024-02-05 16:25:51 +0330

    add new fields

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['plyr'] = '
     {type_legend},type,headline;
+    {source_legend},playerSRC;
+    {player_legend},playerSize,playerOptions,playerStart,playerStop,playerCaption,playerPreload;
+    {poster_legend:hide},posterSRC;
     {template_legend:hide},customTpl;
     {protected_legend:hide},protected;
     {expert_legend:hide},guests,cssID;
