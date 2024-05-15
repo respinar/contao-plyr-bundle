77aff84ede2d834faa9f7fc8c113e037c09c53b1
Hamid Peywasti <hamid@respinar.com>
2024-05-15 08:03:40 +0330

    add playerCaption

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -7,7 +7,8 @@ use Respinar\ContaoPlyrBundle\Controller\ContentElement\PlyrController;
 $GLOBALS['TL_DCA']['tl_content']['palettes'][PlyrController::TYPE] = '
     {type_legend},type,headline;
     {source_legend},playerSRC;
-    {player_legend},playerSize,playerOptions,playerStart,playerStop,playerCaption,playerPreload;
+    {texttrack_legend},textTrackSRC;
+    {player_legend},playerOptions,playerSize,playerPreload,playerCaption,playerStart,playerStop;
     {poster_legend:hide},posterSRC;
     {template_legend:hide},customTpl;
     {protected_legend:hide},protected;
