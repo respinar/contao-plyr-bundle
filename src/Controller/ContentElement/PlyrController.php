8a16a841d6b1c1b7cf15fc601dc541934c81d595
Hamid Peywasti <hamid@respinar.com>
2025-01-02 19:29:20 +0330

    add identifier so add new schema

--- src/Controller/ContentElement/PlyrController.php
+++ src/Controller/ContentElement/PlyrController.php
@@ -286,7 +286,8 @@ class PlyrController extends AbstractContentElementController
         // Basic schema structure
         $schema = [
             '@type' => $schemaType,
-            'name' => !empty($localizedMetadata?->getTitle()) ? $localizedMetadata->getTitle() : pathinfo($firstFile->getName(), PATHINFO_FILENAME),
+            'identifier' => '#/schema/file/' . $sourceFile->getUuid(),
+            'name' => !empty($localizedMetadata?->getTitle()) ? $localizedMetadata->getTitle() : pathinfo($sourceFile->getName(), PATHINFO_FILENAME),
             'description' => !empty($localizedMetadata?->getAlt()) ? $localizedMetadata->getAlt() : $localizedMetadata?->getCaption() ?? '',
             'contentUrl' => (string) $this->publicUriByStoragePath[$firstFile->getPath()],
             'uploadDate' => date('c', $firstFile->getLastModified()),
